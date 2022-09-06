5702fa825811c99897825658b44e031f8dbd0562
Ryan <rm@test.com>
2022-09-06 13:23:22 -0500

    in class changes

--- avg_steps.py
+++ avg_steps.py
@@ -5,44 +5,30 @@ def main():
     infile = open('steps.csv', 'r')
     outfile = open('avg_steps.csv', 'w')
 
-    running_total = 0
+    running_total = 0.0
     day_counter = 0
     month_counter = 1
+    a = 0
     avg = [[0, 0]] * 12
     reader = csv.reader(infile, delimiter=',')
     next(reader)
     
 
-    for row in reader:
-        if row[month_counter] == str(1):
+    for record in reader:
+        if int(record[0]) == month_counter:
+            running_total += float(record[1])
             day_counter +=1
-            avg[month_counter][1] = running_total + row[1]
+        else:
+            #print(running_total)
+            #print(day_counter)
+            month_avg = running_total / day_counter
+            print(month_avg)
+            avg[a][1] = month_avg
+            day_counter = 0
+            month_counter += 1
+            running_total = 0
+            month_avg = 0
+            a += 1
     print(avg)
             
-    # for row in reader:
-    #     print(row)
-    #     if int(row[0]) == 1:
-    #         running_total += int(row[1])
-    #         avg[0] = 'January'  
-    #         jan_avg = running_total/31
-    #         avg[1] = jan_avg
-
-    # running_total = 0
-    # for row in reader:
-    #     if int(row[0]) == 2:
-    #         running_total += int(row[1])
-    #         avg[2] = 'February'  
-    #         feb_avg = running_total/28
-    #         avg[3] = feb_avg
-
-    # running_total = 0
-    # for row in reader:
-    #     if int(row[0]) == 3:
-    #         running_total += int(row[1])
-    #         avg[4] = 'March'  
-    #         mar_avg = running_total/31
-    #         avg[5] = mar_avg    
-    
-    # print(avg)
-
 main()
--- employee_bonus.py
+++ employee_bonus.py
@@ -8,12 +8,11 @@ def main():
     next(csvfile)
 
     for record in csvfile:
-        #print('ID:', record[0], 'Name:', record[1], record[2], 'Salary:', record[3], 'Bonus:', record[4])
-        print('Employee ID:', record[0])
+        print('Employee ID:  ', record[0])
         print('Employee Name:', record[1], record[2])
         bonus_amt = float(record[3]) * float(record[4])
         total_salary = float(record[3]) + bonus_amt
-        print('Total Salary:', total_salary)
+        print('Total Pay:     ', '$', format(total_salary, ',.2f'), sep='')
         print()
         input('Press enter for next employee.')
         print()
